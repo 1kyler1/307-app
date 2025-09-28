f13fa2b6a8d1de87323be5ffcc50088fa74acfc4
Kyler <offersknord@gmail.com>
2025-09-27 22:27:12 -0700

    feat: accessed characters through character props

--- packages/react-frontend/src/Table.jsx
+++ packages/react-frontend/src/Table.jsx
@@ -1,5 +1,4 @@
 // src/Table.jsx
-// src/Table.jsx
 function TableHeader() {
     return (
       <thead>
@@ -10,39 +9,29 @@ function TableHeader() {
       </thead>
     );
   }
-  
-  function TableBody() {
+function TableBody(props) {
+    const rows = props.characterData.map((row, index) => {
+      return (
+        <tr key={index}>
+          <td>{row.name}</td>
+          <td>{row.job}</td>
+        </tr>
+      );
+     }
+    );
     return (
-      <tbody>
-        <tr>
-          <td>Charlie</td>
-          <td>Janitor</td>
-        </tr>
-        <tr>
-          <td>Mac</td>
-          <td>Bouncer</td>
-        </tr>
-        <tr>
-          <td>Dee</td>
-          <td>Aspiring actress</td>
-        </tr>
-        <tr>
-          <td>Dennis</td>
-          <td>Bartender</td>
-        </tr>
-      </tbody>
+        <tbody>
+          {rows}
+         </tbody>
+     );
+  }
+  
+function Table(props) {
+    return (
+        <table>
+          <TableHeader />
+          <TableBody characterData={props.characterData} />
+        </table>
     );
   }
-
-  function Table() {
-    return (
-      <table>
-        <TableHeader />
-        <TableBody />
-      </table>
-    );
-}
-
-export default Table;
-  
-  
+  export default Table;
