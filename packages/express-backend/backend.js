739a952b36977eba639ece304d0ed761ee070d99
Kyler <offersknord@gmail.com>
2025-10-01 15:18:31 -0700

    feat: added find users by name function

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -32,12 +32,34 @@ const users = {
         id: "zap555",
         name: "Dennis",
         job: "Bartender"
+      },
+      {
+        id: "zap555",
+        name: "Lebron",
+        job: "Jameson"
       }
     ]
 };
 
-app.get("/users", (req, res) => {
-    res.send(users);
+// app.get("/users", (req, res) => {
+//     res.send(users);
+//   });
+
+  const findUserByName = (name) => {
+    return users["users_list"].filter(
+      (user) => user["name"] === name
+    );
+  };
+  
+  app.get("/users", (req, res) => {
+    const name = req.query.name;
+    if (name != undefined) {
+      let result = findUserByName(name);
+      result = { users_list: result };
+      res.send(result);
+    } else {
+      res.send(users);
+    }
   });
 
 app.listen(port, () => {
