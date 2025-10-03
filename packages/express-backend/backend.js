28471d285a2556b544947f9762b703c3164c32dd
Kyler <offersknord@gmail.com>
2025-10-03 00:23:59 -0700

    feat: added delete and ger users givedn job and name

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -41,42 +41,64 @@ const users = {
     ]
 };
 
-// app.get("/users", (req, res) => {
-//     res.send(users);
-//   });
-
-  const findUserByName = (name) => {
-    return users["users_list"].filter(
-      (user) => user["name"] === name
-    );
-  };
-  
-  app.get("/users", (req, res) => {
-    const name = req.query.name;
-    if (name != undefined) {
-      let result = findUserByName(name);
-      result = { users_list: result };
-      res.send(result);
-    } else {
-      res.send(users);
-    }
+const findUsers = ({ name, job }) =>
+  users.users_list.filter(u => {
+    const matchName = name ? u.name === name : true;
+    const matchJob = job ? u.job === job : true;
+    return matchName && matchJob;
   });
+app.get("/users", (req, res) => {
+    const { name, job } = req.query;
+    if (name !== undefined || job !== undefined) {
+        const result = findUsers({ name, job });
+    return res.send({ users_list: result });
+    }
+    return res.send(users);
+});
 
 const findUserById = (id) =>
     users["users_list"].find((user) => user["id"] === id);
 
 app.get("/users/:id", (req, res) => {
-  const id = req.params["id"]; //or req.params.id
-  let result = findUserById(id);
-  if (result === undefined) {
-    res.status(404).send("Resource not found.");
-  } else {
-    res.send(result);
-  }
+    const id = req.params["id"]; //or req.params.id
+    let result = findUserById(id);
+    if (result === undefined) {
+        res.status(404).send("Resource not found.");
+    } else {
+        res.send(result);
+    }
+});
+
+const removeUsersById = (id) => {
+    const before = users.users_list.length;
+    users.users_list = users.users_list.filter((u) => u.id !== id);
+    const after = users.users_list.length;
+    return before - after; // number of records deleted
+};
+
+app.delete("/users/:id", (req, res) => {
+    const { id } = req.params;
+    const deletedCount = removeUsersById(id);
+  
+    if (deletedCount === 0) {
+      return res.status(404).send("Resource not found.");
+    }
+    return res.status(204).send();
+  });
+
+const addUser = (user) => {
+    users["users_list"].push(user);
+    return user;
+};
+  
+app.post("/users", (req, res) => {
+    const userToAdd = req.body;
+    addUser(userToAdd);
+    res.send();
 });
 
 app.listen(port, () => {
   console.log(
     `Example app listening at http://localhost:${port}`
   );
-});
+}); 
