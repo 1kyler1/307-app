1fc96a24c83d74d88bbc84c1db568a900581f968
Kyler <offersknord@gmail.com>
2025-10-05 23:25:52 -0700

    feat: backend updates with frontend

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -14,8 +14,12 @@ const initialCharacters = [
 function MyApp() {
   const [characters, setCharacters] = useState([]);
 
-  function updateList(person) {
-    setCharacters([...characters, person]);
+  function updateList(person) { 
+    postUser(person)
+      .then(() => setCharacters([...characters, person]))
+      .catch((error) => {
+        console.log(error);
+      })
   }
 
   function removeOneCharacter(index) {
@@ -24,6 +28,18 @@ function MyApp() {
 
   function fetchUsers() {
     const promise = fetch("http://localhost:8000/users");
+    return promise;
+  }
+
+  function postUser(person) {
+    const promise = fetch("Http://localhost:8000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(person),
+    });
+
     return promise;
   }
 
